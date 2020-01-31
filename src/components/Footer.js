de9d42d5616f50d2758c7e9652fe38dd17ee9cc3
dastrong <danielstrong.ds@gmail.com>
2020-01-31 16:32:59 +0900

    edited footer to use svgs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,34 +1,25 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { ReactComponent as FacebookSVG } from '../img/social/facebook.svg';
+import { ReactComponent as InstagramSVG } from '../img/social/instagram.svg';
 
-import facebook from '../img/social/facebook.svg';
-import instagram from '../img/social/instagram.svg';
+const SocialLink = ({ url, children }) => (
+  <a href={url} target="_blank" rel="noopener noreferrer">
+    {children}
+  </a>
+);
 
 const Footer = () => (
   <footer className="footer">
     <Link to="/">Home</Link>
-    <a
-      href="https://www.instagram.com/fwarrior.135"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <img
-        src={instagram}
-        alt="Instagram"
-        style={{ width: '1em', height: '1em' }}
-      />
-    </a>
-    <a
-      href="https://www.facebook.com/walter.barrios"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <img
-        src={facebook}
-        alt="Facebook"
-        style={{ width: '1em', height: '1em' }}
-      />
-    </a>
+
+    <SocialLink url="https://www.instagram.com/fwarrior.135">
+      <InstagramSVG height="1em" width="1em" />
+    </SocialLink>
+
+    <SocialLink url="https://www.facebook.com/walter.barrios">
+      <FacebookSVG height="1em" width="1em" />
+    </SocialLink>
 
     <p>
       Built by{' '}
