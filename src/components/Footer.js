141ff106c13b3874afbe0c7b979756c4517f9e09
dastrong <danielstrong.ds@gmail.com>
2020-01-31 16:15:04 +0900

    basic footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,100 +3,45 @@ import { Link } from 'gatsby';
 
 import facebook from '../img/social/facebook.svg';
 import instagram from '../img/social/instagram.svg';
-import twitter from '../img/social/twitter.svg';
-import vimeo from '../img/social/vimeo.svg';
 
-const Footer = class extends React.Component {
-  render() {
-    return (
-      <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
-            <div className="columns">
-              <div className="column is-4">
-                <section className="menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/about">
-                        About Me
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Blog
-                      </Link>
-                    </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4">
-                <section>
-                  <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact">
-                        Contact
-                      </Link>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
-    );
-  }
-};
+const Footer = () => (
+  <footer className="footer">
+    <Link to="/">Home</Link>
+    <a
+      href="https://www.instagram.com/fwarrior.135"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        src={instagram}
+        alt="Instagram"
+        style={{ width: '1em', height: '1em' }}
+      />
+    </a>
+    <a
+      href="https://www.facebook.com/walter.barrios"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        src={facebook}
+        alt="Facebook"
+        style={{ width: '1em', height: '1em' }}
+      />
+    </a>
+
+    <p>
+      Built by{' '}
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.github.com/dastrong"
+      >
+        DaStrong
+      </a>
+      .
+    </p>
+  </footer>
+);
 
 export default Footer;
