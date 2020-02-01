81739eb85b2135978a3b9062b6b65d6b5778a29f
Daniel Strong <danielstrong.ds@gmail.com>
2020-02-01 18:32:16 +0900

    added console log to see what netlify images contain

--- src/templates/index-page.tsx
+++ src/templates/index-page.tsx
@@ -55,72 +55,69 @@ export const IndexPageTemplate = (props: TemplateProps) => {
 
   const injuryDate = new Date('12/1/2019').getTime();
 
-  return (
-    <>
-      <Layout>
-        <section className="index-cover">
-          <div className="imgs">
-            {Object.keys(imgs).map((file: 'image1' | 'image2' | 'image3') => {
-              const img = imgs[file].childImageSharp.fluid;
-              const name = img.originalName.slice(
-                0,
-                img.originalName.length - 5
-              );
-              return <Img key={name} fluid={img} alt={name} />;
-            })}
-          </div>
-          <div className="headings">
-            <h1>{heading}</h1>
-            <p>{subheading}</p>
-          </div>
-        </section>
+  console.log(imgs);
 
-        <section className="index-content" style={{ height: '700px' }}>
-          <h1
-            style={{ textAlign: 'center', fontSize: '3rem', marginTop: '40px' }}
-          >
-            Blog Posts
-          </h1>
-          <div className="index-posts">
-            {posts &&
-              posts.edges.map(post => {
-                const { frontmatter, id } = post.node;
-                const { title, date, description } = frontmatter;
-                const daysSinceInjury = Math.floor(
-                  (new Date(date).getTime() - injuryDate) / 86400000 // 1 day in ms
-                );
-                return (
-                  <div className="post__container" key={id}>
-                    <div className="post__day">{`Day ${daysSinceInjury}`}</div>
-                    <div className="post__content">
-                      <h2 className="post__title">{title}</h2>
-                      <span className="post__date">{date}</span>
-                      <p className="post__description">{description}</p>
-                    </div>
+  return (
+    <Layout>
+      <section className="index-cover">
+        <div className="imgs">
+          {Object.keys(imgs).map((file: 'image1' | 'image2' | 'image3') => {
+            const img = imgs[file].childImageSharp.fluid;
+            const name = img.originalName.slice(0, img.originalName.length - 5);
+            return <Img key={name} fluid={img} alt={name} />;
+          })}
+        </div>
+        <div className="headings">
+          <h1>{heading}</h1>
+          <p>{subheading}</p>
+        </div>
+      </section>
+
+      <section className="index-content" style={{ height: '700px' }}>
+        <h1
+          style={{ textAlign: 'center', fontSize: '3rem', marginTop: '40px' }}
+        >
+          Blog Posts
+        </h1>
+        <div className="index-posts">
+          {posts &&
+            posts.edges.map(post => {
+              const { frontmatter, id } = post.node;
+              const { title, date, description } = frontmatter;
+              const daysSinceInjury = Math.floor(
+                (new Date(date).getTime() - injuryDate) / 86400000 // 1 day in ms
+              );
+              return (
+                <div className="post__container" key={id}>
+                  <div className="post__day">{`Day ${daysSinceInjury}`}</div>
+                  <div className="post__content">
+                    <h2 className="post__title">{title}</h2>
+                    <span className="post__date">{date}</span>
+                    <p className="post__description">{description}</p>
                   </div>
-                );
-              })}
-          </div>
-          <div className="about-container">
-            <p>{aboutme}</p>
-            <hr />
-            <p>{injuryinfo}</p>
-            <hr />
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Cupiditate, eius quo ad dolor quaerat corporis, voluptatem ratione
-              quasi quae cumque provident nihil? Magni labore maxime, illo
-              fugiat maiores deleniti nam.
-            </p>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis
-              aspernatur reiciendis quae quam ratione esse eum repellendus eos.
-              Laboriosam non soluta nulla.
-            </p>
-          </div>
-        </section>
-      </Layout>
-    </>
+                </div>
+              );
+            })}
+        </div>
+        <div className="about-container">
+          <p>{aboutme}</p>
+          <hr />
+          <p>{injuryinfo}</p>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate,
+            eius quo ad dolor quaerat corporis, voluptatem ratione quasi quae
+            cumque provident nihil? Magni labore maxime, illo fugiat maiores
+            deleniti nam.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis
+            aspernatur reiciendis quae quam ratione esse eum repellendus eos.
+            Laboriosam non soluta nulla.
+          </p>
+        </div>
+      </section>
+    </Layout>
   );
 };
 
