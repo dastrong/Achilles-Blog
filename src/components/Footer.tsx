eee6a978516ac0710f6486166d96335585ebd500
Daniel Strong <danielstrong.ds@gmail.com>
2020-02-01 21:41:42 +0900

    switched to typescript and use img tag instead of svg now

---
+++ src/components/Footer.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import facebook from '../img/social/facebook.svg';
+import instagram from '../img/social/instagram.svg';
+
+type SocialTypes = {
+  img: string;
+  name: string;
+  url: string;
+};
+
+const SocialLink = ({ img, name, url }: SocialTypes) => (
+  <a href={url} target="_blank" rel="noopener noreferrer">
+    <img src={img} alt={name} style={{ width: '1em', height: '1em' }} />
+  </a>
+);
+
+const Footer = () => (
+  <footer className="footer">
+    <Link to="/">Home</Link>
+
+    <SocialLink
+      img={facebook}
+      name="facebook"
+      url="https://www.instagram.com/fwarrior.135"
+    />
+
+    <SocialLink
+      img={instagram}
+      name="instagram"
+      url="https://www.facebook.com/walter.barrios"
+    />
+
+    <p>
+      Built by{' '}
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.github.com/dastrong"
+      >
+        DaStrong
+      </a>
+      .
+    </p>
+  </footer>
+);
+
+export default Footer;
