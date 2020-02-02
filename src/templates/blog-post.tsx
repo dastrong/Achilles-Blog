6ebb4fe4517a2a0e7ee3793db8d96369e23bd568
Daniel Strong <danielstrong.ds@gmail.com>
2020-02-02 20:24:37 +0900

    updated blog-post template

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,80 +1,123 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import Layout from '../components/Layout';
 import Content, { HTMLContent } from '../components/Content';
 import useHelmet from '../components/useHelmet';
+import { getInjuryDate, getDaysSinceInjury } from '../utils/dates';
+
+type NextPrevProps = {
+  frontmatter: {
+    date: Date;
+  };
+  fields: {
+    slug: string;
+  };
+};
+
+type TemplateProps = {
+  content: any;
+  contentComponent?: any;
+  date: string;
+  title: string;
+  helmet?: JSX.Element;
+  previous?: NextPrevProps;
+  next?: NextPrevProps;
+};
 
 export const BlogPostTemplate = ({
   content,
   contentComponent,
-  description,
+  date,
   title,
   helmet,
-}) => {
+  previous,
+  next,
+}: TemplateProps) => {
   const PostContent = contentComponent || Content;
-
-  return (
-    <section className="section">
-      {helmet || ''}
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
-            <p>{description}</p>
-            <PostContent content={content} />
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
-
-// BlogPostTemplate.propTypes = {
-//   content: PropTypes.node.isRequired,
-//   contentComponent: PropTypes.func,
-//   description: PropTypes.string,
-//   title: PropTypes.string,
-//   helmet: PropTypes.object,
-// };
-
-const BlogPost = ({ data }) => {
-  const { markdownRemark: post } = data;
-  const { title, description } = post.frontmatter;
-  const helmet = useHelmet(title, description);
+  const injuryDate = getInjuryDate();
 
   return (
     <Layout>
-      <BlogPostTemplate
-        content={post.html}
-        contentComponent={HTMLContent}
-        description={description}
-        helmet={helmet}
-        title={title}
-      />
+      {helmet || ''}
+      <section className="blogpost">
+        <div className="blogpost__container">
+          <div className="blogpost__info">
+            <h1>{title}</h1>
+            <p>{new Date(date).toDateString()}</p>
+          </div>
+          <PostContent content={content} />
+          {/* {(previous || next) && (
+            <div className="blogpost__pagination">
+              {previous && (
+                <Link to={previous.fields.slug}>
+                  {`Day ${getDaysSinceInjury(
+                    injuryDate,
+                    new Date(previous.frontmatter.date)
+                  )}`}
+                </Link>
+              )}
+              {next && (
+                <Link to={next.fields.slug}>
+                  {`Day ${getDaysSinceInjury(
+                    injuryDate,
+                    new Date(next.frontmatter.date)
+                  )}`}
+                </Link>
+              )}
+            </div>
+          )} */}
+        </div>
+      </section>
     </Layout>
   );
 };
 
-// BlogPost.propTypes = {
-//   data: PropTypes.shape({
-//     markdownRemark: PropTypes.object,
-//   }),
-// };
+type Props = {
+  data: {
+    post: {
+      id: string;
+      html: React.ReactNode;
+      frontmatter: {
+        date: string;
+        title: string;
+        description: string;
+      };
+    };
+  };
+  pageContext: {
+    next: NextPrevProps;
+    previous: NextPrevProps;
+  };
+};
 
-export default BlogPost;
+export default function BlogPost({ data, pageContext }: Props) {
+  const { html } = data.post;
+  const { title, description, date } = data.post.frontmatter;
+  const helmet = useHelmet(title, description);
+  const { next, previous } = pageContext;
+
+  return (
+    <BlogPostTemplate
+      content={html}
+      contentComponent={HTMLContent}
+      helmet={helmet}
+      title={title}
+      date={date}
+      next={next}
+      previous={previous}
+    />
+  );
+}
 
 export const pageQuery = graphql`
   query BlogPostByID($id: String!) {
-    markdownRemark(id: { eq: $id }) {
+    post: markdownRemark(id: { eq: $id }) {
       id
       html
       frontmatter {
-        date(formatString: "MMMM DD, YYYY")
+        date
         title
         description
-        tags
       }
     }
   }
